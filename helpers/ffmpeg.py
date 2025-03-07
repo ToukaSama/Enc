f75fb96f07059f32d2d65e8b8d5d9ad31ee68afa
ToukaSama <blacksummoner6659@gmail.com>
2025-03-07 20:57:53 +0530

    Update ffmpeg.py

--- helpers/ffmpeg.py
+++ helpers/ffmpeg.py
@@ -12,10 +12,10 @@ async def enc_video(client, message, data):
     await message.edit_text(f"`{dwld_loc}`\n\nEncoding Video ...\n\n**please wait**")
 
     out_loc = os.path.splitext(dwld_loc)[0]
-    out_loc = out_loc + "_320p.mp4"
+    out_loc = out_loc + ".mkv"
     
     #out, err, rcode, pid = await execute(f"ffmpeg -i \"{dwld_loc}\" -map 0:{data['map']} -af \"pan=stereo|c0=c0|c1=c0\" -ar 48000 -ab 256k -f mp3 \"{out_loc}\" -y")
-    out, err, rcode, pid = await execute(f"ffmpeg -i \"{dwld_loc}\" -c:v libx264 -crf 20 -s 320*240 -c:a aac -ar 48000 -ab 96k \"{out_loc}\" -y")
+    out, err, rcode, pid = await execute(f"ffmpeg -i \"{dwld_loc}\" -c:v libx265 -crf 30 -s 854*480 -c:a libopus -ar 48000 -ab 96k \"{out_loc}\" -y")
     if rcode != 0:
         await message.edit_text(f"**(320p mp4 96k) - Error Occured.\n\n{err}**")
         print(err)
